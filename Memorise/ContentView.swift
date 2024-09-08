2ceed40f3115b664cc340e3fa4e7cb80de1a23ca
Valentyna <gerry29072001@gmail.com>
2024-09-08 23:00:31 +0300

    Finished RT 1 of assignment

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -9,19 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let emojis: [String] = ["👻","🎃","🕷️","😈"]
+    let emojis: [String] = ["👻","🎃","🕷️","😈", "💀", "🕸️", "🧙", "🙀", "👹", "😱", "☠️", "🍭"]
+    
+    @State var cardCount: Int = 4
     
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self)  { index in
-                CardView(content: emojis[index])
-            }
+        ScrollView {
+            cards
+            Spacer()
+            cardCountAdjusters
         }
-        .foregroundColor(.purple)
             .padding()
-            
+        }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self)  { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+            .foregroundColor(.purple)
+        }
+        
+    }
+    
+    var cardAdder: some View {
+        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.fill.badge.plus")
+            .imageScale(.large)
+            .font(.largeTitle)
+            .foregroundColor(.yellow)
+    }
+    
+    var cardRemoval: some View {
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+        .imageScale(.large)
+        .font(.largeTitle)
+        .foregroundColor(.yellow)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardAdder
+            Spacer()
+            cardRemoval
         }
     }
+}
+
+
 
 struct CardView: View {
     let content: String
@@ -29,15 +73,14 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
-            if faceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content)
                     .font(.largeTitle)
-            } else {
-                    base.foregroundColor(.purple)
-                    base.fill()
             }
+            .opacity(faceUp ? 1 : 0)
+            base.fill().opacity(faceUp ? 0 : 1)
         }
         .onTapGesture {
             faceUp.toggle()
