d2dbe997c8e9879f26f6e140fcb38321bcf0e7de
Valentyna <gerry29072001@gmail.com>
2024-09-08 20:18:28 +0300

    Initial Commit

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Valentyna Kharkova on 08.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
